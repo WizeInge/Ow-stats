2859c6b8b48f5f7b5fe1eb157a60d4e737c17c0a
WizeInge <thomas.burgio@ynov.com>
2016-12-15 13:50:47 +0100

    Adding first stats, fix css & laoding page js

--- www/js/core.js
+++ www/js/core.js
@@ -14,38 +14,45 @@ var verifyBtag = function() {
     var page = event.target;
     var btag = document.getElementById('btag').value;
     var nBtag = btag.replace('#', '-');
+    var verif = new RegExp(/^[A-Za-z0-9]{2,11}#[0-9]{4,5}$/u);
 
     $('#wait').show();
 
-    if(nBtag == ""){
+    if(btag == ""){
       showDialog('dialog-emptyBtag');
       $('#wait').hide();
       return;
     }
-
-    var req = new XMLHttpRequest();
-    req.open('GET', 'https://api.lootbox.eu/pc/eu/' + nBtag + '/profile', true);
-    req.send(null);
-    req.onreadystatechange = function (aEvt) {
-      if (req.readyState == 4) {
-        if(req.status == 200){
-            var json = JSON.parse(req.responseText);
-            if(json.statusCode == 404){
-              showDialog('dialog-errorBtag');
-              $('#wait').hide();
-              return;
-            }
+    else{
+      if(verif.test(btag)){
+        $.ajax({
+          url: 'https://api.lootbox.eu/pc/eu/' + nBtag + '/profile',
+          type: 'GET',
+          success: function(json){
             document.querySelector('#navigator').pushPage('page2.html');
-        }
-        else{
-          showDialog('dialog-error');
-          $('#wait').hide();
-        }
+            construcPage2(json);
+            $('#wait').hide();
+          }
+        });
       }
-    };
-
-    /*const regex = /^[\p{L}\p{Mn}][\p{L}\p{Mn}0-9]{2,11}#[0-9]{4,5}/u;
-    if (regex.exec(btag) !== null) {
-      document.querySelector('#navigator').pushPage('page2.html');
-    }*/
+      else{
+        showDialog('dialog-emptyBtag');
+        $('#wait').hide();
+        return;
+      }
+    }
 };
+
+var construcPage2 = function(json){
+  setTimeout(function(){
+    $('#wait2').hide();
+    var ratio = json.data.games.competitive.wins / json.data.games.competitive.lost;
+    ratio = Math.round(ratio*100)/100;
+    $('#dataRet').append('<div class="tcenter"><img src="' + json.data.avatar + '" class="iconPlayer"/>' +
+      '<p>'+ json.data.username + ' lvl ' + json.data.level + '</p>' +
+      '<img src="' + json.data.competitive.rank_img + '" class="iconRank"/><p style="margin-bottom:20px;">' + json.data.competitive.rank + ' - ' + json.data.playtime.competitive + ' of play.</p></div>' +
+      '<div class="divider"></div>' +
+      '<h1 class="tcenter">Competitve</h1><div class="gridDiv">'+
+      '<p>Played : ' + json.data.games.competitive.played + '</p><p><span style="color:#3fa53f">Wins</span> : ' + json.data.games.competitive.wins + '</p><p><span style="color:#8a2a2a">Lost</span> : ' + json.data.games.competitive.lost + '</p><p>Ratio (W/L) : ' + ratio + '</p></div>');
+  }, 500)
+}
